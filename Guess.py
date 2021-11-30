c2d94e3a5735f8b9cbe4111f71da4ac4bf6cb4cb
Jay Rivs <92488611+thesageRR3@users.noreply.github.com>
2021-11-30 01:59:43 -0500

    Add files via upload
    This is a simple guessing game, there is no randomization (yet)
    - Goal_1: practice using conditional statements
    - Goal_2: practice using functions & loops

---
+++ Guess.py
@@ -0,0 +1,33 @@
+print("I'm thinking of a number between 1 and 10")
+print("Can you guess what the number is?")
+guess = int(input("Please enter your guess: "))
+
+def am_I_right(guess):
+    if guess == 3:
+        print("CONGRATULATIONS! How did you know?")
+    elif (guess > 10):
+        print("Remember we're guessing between 1  - 10")
+    elif guess == 10:
+        print("WRONG!")
+    elif (guess == 2) or (guess == 4):
+        print("You're really close!")
+    else:
+        print("That's not quite it, try again!")
+    return am_I_right
+
+am_I_right(guess)
+
+counter = 0
+
+while guess != 3:
+    counter = counter +1
+    if counter == 1:
+        print(f"You have taken {counter} guess so far!")
+    elif counter > 2:
+        print("You're really not that good at this, huh?")
+        print("That's {counter} guesses so far!")
+        print("Here's a hint, it comes after 2!")
+    else:
+        print(f"You have taken {counter} guesses so far!")     
+    guess = int(input("Please enter your guess: "))
+    am_I_right(guess) 
