4f989c12edc86c9c9a2d7d2cf7fd11ac950d3cfb
Jay Rivs <92488611+thesageRR3@users.noreply.github.com>
2021-11-30 01:58:32 -0500

    Add files via upload
    This is a simple factorial program
    Goal_01: Find the factor of a given number using conditional statements within a loop
    Goal_02: Add the ability to continue or end the program

---
+++ Factors.py
@@ -0,0 +1,20 @@
+print("Today's goal is to factor numbers")
+number = int(input("Enter a positive interger: "))
+
+def factored(number):
+    for n in range(1, number +1):
+        factorial = number % n
+        if factorial == 0:
+            print(f"{n} is a factor of {number}")
+    nextOrder = True
+    while nextOrder:
+        nextOrder = (input("hit \"A\" to go again or \"Q\" to quit: ")).lower()
+        if nextOrder == "a":
+            number = int(input("Enter a positive interger: "))
+            factored(number)
+        elif nextOrder == "q":
+            nextOrder = False
+            print("Thank you for using this program")
+    return factored
+
+factored(number)
